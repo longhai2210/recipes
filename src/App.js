89172d2d197398d5c15df3fc9add07b9ebf01482
qe <qe@qes-Mac-mini.local>
2019-06-27 16:49:13 +0700

    push 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {recipes} from './tempList';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    recipes: recipes,
+    url:"https://www.food2fork.com/api/search?key=e0e7125952c1518d56d4adfbee43886b"
+  };
+
+  /* async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes:jsonData.recipes
+      });
+    } catch(error) {
+      console.log(error);
+    }  
+  }
+
+  componentDidMount() {
+    this.getRecipes()
+  } */
+
+  render() {
+    console.log(this.state.recipes);
+
+    return (
+      <React.Fragment>
+        <RecipeList recipes={this.state.recipes} />
+        <RecipeDetails />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {recipe} from '../tempDetails';
+
+export default class RecipeDetails extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Details</h1>
+            </React.Fragment>
+        )
+    }
+}
